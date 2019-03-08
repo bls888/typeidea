92faeb27d83e296191aba14bd40368aa8164199f
wajpzywj <wajpzywj@qq.com>
2019-03-08 14:58:17 +0800

    seconde commit for resume

--- typeidea/wsgi.py
+++ typeidea/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-# nihao
+
 from django.core.wsgi import get_wsgi_application
 
 profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
